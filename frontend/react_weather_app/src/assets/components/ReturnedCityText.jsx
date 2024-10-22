70f45f15c40d624eb3febb55ee1c66b44c8f7bb9
Shubham Sharma <87312848+shubhams-git@users.noreply.github.com>
2024-10-23 08:28:52 +1100

    minor UI enhancements

--- frontend/react_weather_app/src/assets/components/ReturnedCityText.jsx
+++ frontend/react_weather_app/src/assets/components/ReturnedCityText.jsx
@@ -2,20 +2,20 @@ export const ReturnedCityText = ({ city, onBack }) => {
     return (
       <div className="flex justify-center">
         <div className="flex justify-center items-end font-serif text-white">
-            <div className="text-2xl">
+            <div className="text-xl">
                 The Selected City is 
             </div>
-            <div className="text-4xl mt-1 pl-2 pr-5 font-extrabold from-indigo-500 to-blue-500 bg-gradient-to-l bg-clip-text text-transparent">
+            <div className="text-3xl mt-1 pl-2 pr-5 font-extrabold from-indigo-500 to-blue-500 bg-gradient-to-l bg-clip-text text-transparent">
                 {city}
             </div>
 
         </div>
         <div className="flex items-end">
-            <button onClick={onBack} className="p-1 text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 px-5 my-1 text-sm inline-flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <button onClick={onBack} className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 pl-2 pr-3 mb-1.5 text-sm inline-flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
             </svg>
-                <div className=" flex items-end text-lg text-white pl-2">
+                <div className=" flex items-end text-base text-white pl-2 text-opacity-75">
                     Change City
                 </div>
             </button> 
