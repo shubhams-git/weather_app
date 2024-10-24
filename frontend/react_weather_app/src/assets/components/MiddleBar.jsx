40d741c27d01a1d317cb49a06035b4008276a165
Shubham Sharma <87312848+shubhams-git@users.noreply.github.com>
2024-10-24 15:52:30 +1100

    Added navbar UI effects

--- frontend/react_weather_app/src/assets/components/MiddleBar.jsx
+++ frontend/react_weather_app/src/assets/components/MiddleBar.jsx
@@ -1,16 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const MiddleBar = () => {
+  const location = useLocation(); // Get the current route
+
+  const isActive = (path) => location.pathname === path;
+
   return (
     <div className="flex justify-center text-white text-lg font-semibold w-1/2">
-      <div className="content-center px-20">
-        <Link to="/">Home</Link>
+      <div className={`content-center px-20`}>
+        <Link to="/" className={isActive("/") ? "text-[#3498db] font-bold" : "text-gray hover:text-[#50b9ff] transition-all duration-3000"}>Home</Link>
       </div>
-      <div className="content-center px-20">
-        <Link to="/graph">Graph</Link>
+      <div className={`content-center px-20`}>
+        <Link to="/graph" className={isActive("/graph") ? "text-[#3498db] font-bold" : "text-gray-300 hover:text-[#50b9ff]  transition-all duration-3000"}>Graph</Link>
       </div>
-      <div className="content-center px-20">
-        <Link to="/about">About</Link>
+      <div className={`content-center px-20`}>
+        <Link to="/about" className={isActive("/about") ? "text-[#3498db] font-bold" : "text-gray-300 hover:text-[#50b9ff]  transition-all duration-3000"}>About</Link>
       </div>
     </div>
   );
