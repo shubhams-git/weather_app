867a5cfc9a02b079d4f318ae8982f4f2834b483f
Shubham Sharma <87312848+shubhams-git@users.noreply.github.com>
2025-05-25 16:12:12 +1000

    Improved the backend notice

--- frontend/react_weather_app/src/assets/components/BackendStatusNotice.jsx
+++ frontend/react_weather_app/src/assets/components/BackendStatusNotice.jsx
@@ -15,7 +15,7 @@ export const BackendStatusNotice = () => {
     showNotice && (
       <div className="w-full bg-yellow-100 border-b border-yellow-300 text-yellow-800 text-center py-3 z-50">
         <p>
-          <strong>Notice:</strong> Our server may take up to 2 minutes to respond if it’s just starting up. Thanks for your patience!
+          <strong>Notice:</strong> The server may experience a brief cold start delay of up to 2 minutes if it has been idle. Thank you for your understanding.
         </p>
       </div>
     )
