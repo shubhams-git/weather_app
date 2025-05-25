a8d8074ca0e6755faea4a5cc59c1c14028ed1bec
Shubham Sharma <87312848+shubhams-git@users.noreply.github.com>
2025-05-25 16:04:34 +1000

    Added the backend notice

--- frontend/react_weather_app/src/App.jsx
+++ frontend/react_weather_app/src/App.jsx
@@ -64,7 +64,7 @@ export const App = () => {
   return (
     <Router>
       <div className="z-20 relative min-h-screen w-full bg-slate-950">
-        {/* <BackendStatusNotice /> Display the backend delay notice */}
+        <BackendStatusNotice />
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] -z-10"></div>
         <div className="relative flex flex-col">
           <AppBar />
